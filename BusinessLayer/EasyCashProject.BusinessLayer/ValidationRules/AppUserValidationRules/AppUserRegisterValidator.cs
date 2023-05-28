2559ad06fe88970111675be87e3565d0eb74f177
samet <74676437+sametyorgunn@users.noreply.github.com>
2023-05-29 01:22:57 +0300

    validators and Dal

---
+++ BusinessLayer/EasyCashProject.BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,24 @@
+﻿using EasyCashProject.DtoLayer.Dtos.AppUserDto;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashProject.BusinessLayer.ValidationRules.AppUserValidationRules
+{
+    public class AppUserRegisterValidator:AbstractValidator<AppUserRegisterDto>
+    {
+        public AppUserRegisterValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("İsim Boş Geçilemez!");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Mail Boş Geçilemez!");
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("Kullanıcı Adı Boş Geçilemez!");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Şifre Boş Geçilemez!");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Şifre Tekrarı Boş Geçilemez!");
+            RuleFor(x => x.ConfirmPassword).Equal(y=>y.Password).WithMessage("Şifreler Eşleşmiyor");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Geçerli Bir mail adresi giriniz!");
+        }
+    }
+}
---
+++ DataAccessLayer/EasyCashProject.DataAccessLayer/Repository/GenericRepository.cs
@@ -0,0 +1,56 @@
+﻿using EasyCashProject.DataAccessLayer.Abstract;
+using EasyCashProject.DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashProject.DataAccessLayer.Repository
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        public void Delete(T t)
+        {
+            using(Context c = new Context())
+            {
+                c.Set<T>().Remove(t);
+                c.SaveChanges();
+            }
+        }
+
+        public T GetById(int id)
+        {
+            using (Context c = new Context())
+            {
+               return c.Set<T>().Find(id);
+            }
+        }
+
+        public List<T> GetList()
+        {
+            using (Context c = new Context())
+            {
+               return c.Set<T>().ToList();
+            }
+        }
+
+        public void Insert(T t)
+        {
+            using (Context c = new Context())
+            {
+                c.Set<T>().Add(t);
+                c.SaveChanges();
+            }
+        }
+
+        public void Update(T t)
+        {
+            using (Context c = new Context())
+            {
+                c.Set<T>().Update(t);
+                c.SaveChanges();
+            }
+        }
+    }
+}
