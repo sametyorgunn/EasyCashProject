898e4d91465c10134daba2184d5fd9c3eb065a0c
samet <74676437+sametyorgunn@users.noreply.github.com>
2023-05-28 23:52:37 +0300

    tables and relations

---
+++ DataAccessLayer/EasyCashProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,22 @@
+﻿using EasyCashProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashProject.DataAccessLayer.Concrete
+{
+    public class Context:IdentityDbContext<AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        {
+            optionsBuilder.UseSqlServer("Data Source=SAMET;Initial Catalog=EasyCashProject;Integrated Security=True");
+        }
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<AccountProcess> AccountProcesses { get; set; }
+
+    }
+}
